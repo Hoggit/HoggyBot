8eddffb298458a12de9ffaf4ac2b4d3d37e74010
Dave Lugg <acidictadpole@gmail.com>
2018-05-15 16:28:25 -0400

    Adds commendations. No reading yet

--- commendations/commendations.py
+++ commendations/commendations.py
@@ -17,12 +17,53 @@ class Commendations:
         self.file_path = "data/commendations/commendations.json"
         self.c_commendations = dataIO.load_json(self.file_path)
 
+    def store(self, ctx, commendation):
+        server = ctx.message.server
+        user = commendation['user']
+        if server.id not in self.c_commendations:
+            self.c_commendations[server.id] = {}
+        server_commendations = self.c_commendations[server.id]
+        if user not in server_commendations:
+            server_commendations[user] = []
+        user_commendations = server_commendations[user]
+        user_commendations.append(commendation)
+        server_commendations[user] = user_commendations
+        self.c_commendations[server.id] = server_commendations
+        dataIO.save_json(self.file_path, self.c_commendations)
 
-    async def commend(self, user: discord.Member, text):
-        self.bot.say("Got commendation for user {} with text {}".format(user.name, text))
+    def commendation(self, author, user, text):
+        if author.id == user.id:
+            raise SameUserError()
+        comm = {}
+        comm['user'] = user.id
+        comm['text'] = text
+        comm['author'] = author.id
+        return comm
+
+    @commands.group(name="commend", pass_context=True)
+    async def commend(self, ctx, user: discord.Member, text):
+        author = ctx.message.author
+        try:
+            commendation = self.commendation(author, user, text)
+            print("commendation: {}".format(commendation))
+            self.store(ctx, commendation)
+            await self.bot.say("Commended {}.".format(user.name))
+        except SameUserError:
+            await self.bot.say("You can't give yourself a commendation you nitwit")
 
 
+class SameUserError(Exception):
+    pass
 
+class Commendation:
+
+    def __init__(self, author, user, text):
+        if author.id == user.id:
+            raise SameUserError()
+
+        self.author = author.id
+        self.user = user.id
+        self.text = text
 
 def check_folders():
     if not os.path.exists("data/commendations"):
