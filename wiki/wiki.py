dccf325e7c9371c69f82126ea870ce231d5b7eb3
Dave Lugg <acidictadpole@gmail.com>
2018-05-18 10:18:04 -0400

    Wtf is even async

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -12,30 +12,39 @@ class HoggitWiki:
         self.session = aiohttp.ClientSession()
 
     def url(self, search):
-        return "http://wiki-beta.hoggitworld.com/index.php?search={}&title=Special%3ASearch&profile=default&fulltext=1".format(search)
+        return "http://wiki-beta.hoggitworld.com/index.php?title=Special%3ASearch&search={}&go=Go".format(search)
 
     def fetch(self, session, url):
-        return await session.get(url)
+        return session.get(url)
 
-    def was_redirect(self, response):
+    def was_redirect(self, resp):
+        print("==============================================")
+        print(resp)
+        print("==============================================")
+        print(resp.status)
+        print("++++++++++++++++++++++++++++++++++++++++++++++")
+        print(resp.history)
+        print("----------------------------------------------")
         return len(resp.history) > 0
 
-    def bot_say_single_result(self, result_url):
+    async def bot_say_single_result(self, result_url):
         message = result_url
+        print("Sending message: {}".format(message))
         await self.bot.say(message)
 
 
-    def bot_say_multiple_results(self, response):
+    async def bot_say_multiple_results(self, response):
         message = "Got multiple results. Parsing coming soon"
+        print("Multiple results")
         await self.bot.say(message)
 
     @commands.command()
     async def wiki(self, search_text):
-        resp = await fetch(self.session, url)
-        if (was_redirect(resp)):
-            bot_say_single_result(resp.history[0].url)
+        resp = await self.session.get(self.url(search_text))
+        if (self.was_redirect(resp)):
+            await self.bot_say_single_result(resp.url)
         else:
-            bot_say_multiple_results(resp)
+            await self.bot_say_multiple_results(resp)
 
 def setup(bot):
     bot.add_cog(HoggitWiki(bot))
