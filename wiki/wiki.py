7fd7ce013f9fecbb3afbc3fe636ee4481be750bf
Lugghawk <acidictadpole@gmail.com>
2018-05-19 13:40:08 -0400

    Right imports for checks

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -2,6 +2,7 @@ import os
 import aiohttp
 from bs4 import BeautifulSoup
 from .utils.chat_formatting import pagify
+from .utils import checks
 from .utils.dataIO import fileIO
 from discord.ext import commands
 
