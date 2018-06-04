236f4ce215686c97612c94a692071f422e943184
Dave Lugg <acidictadpole@gmail.com>
2018-06-04 14:48:41 -0400

    Adds wiki alerts.. maybe

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -1,5 +1,9 @@
+import asyncio
 import os
 import aiohttp
+import discord
+import arrow
+import json
 from bs4 import BeautifulSoup
 from .utils.chat_formatting import pagify
 from .utils import checks
@@ -18,6 +22,46 @@ class HoggitWiki:
 
         self.synonyms = fileIO('data/wiki/synonyms.json', 'load')
         self.base_url = "https://wiki.hoggitworld.com"
+        self.recent_changes_url = self.base_url + "/api.php?action=query&list=recentchanges&rcprop=user|title|timestamp&format=json&rctype=edit"
+        self.last_wiki_check = arrow.utcnow()
+        self.alerts = fileIO('data/iki/alerts.json', 'load')
+        self.start_alerts()
+
+    def start_alerts(self):
+        channel = self.alerts["channel"]
+        if channel:
+            print("Starting alerting to channel: " + channel.name)
+            asyncio.ensure_future(self.alert(channel))
+        else:
+            print("No alerts to start")
+
+    def format_recent_changes(self, results):
+        formatted = []
+        for result in results:
+            formatted_result = "{} by {} - {}".format(
+                result["title"],
+                result["user"],
+                arrow.get(result["timestamp"]).humanize()
+                )
+            formatted.append(formatted_result)
+        return formatted
+
+
+    async def alert(self, channel: discord.Channel):
+        await asyncio.sleep(600) #10 minutes
+        timestamp = self.last_wiki_check.format('YYYY-MM-DDTHH:mm:ss')
+        url = self.recent_changes_url + "&rcend=" + timestamp
+        try:
+            response = self.session.get(url)
+            recent_changes = json.loads(await response.text())
+            results = recent_changes["query"]["recentchanges"]
+            formatted_results = self.format_recent_changes(results)
+            self.bot.send_message(channel, formatted_results)
+        except:
+            print("Unexpected error sending wiki recent changes: " + sys.exc_info()[0])
+        finally:
+            if self.alerts["channel"] == channel:
+                asyncio.ensure_future(self.alert(channel))
 
     def url(self, search):
         return self.base_url + "/index.php?title=Special%3ASearch&search={}&go=Go".format(search)
@@ -110,6 +154,16 @@ class HoggitWiki:
         else:
             await self.bot_say_search_results(resp)
 
+    @wiki.command(name="alert", no_pm=True)
+    @checks.mod_or_permissions(manage_server=True)
+    async def _add_alert(self, chan: discord.Channel):
+        """
+        Configures alerts for the hoggit wiki to be sent to the given channel.
+
+        `channel` must be a channel that the bot can send messages to
+        """
+        self.alerts["channel"] = chan
+        self.start_alerts()
 
 def setup(bot):
     if not os.path.exists("data/wiki"):
@@ -119,6 +173,11 @@ def setup(bot):
     f = "data/wiki/synonyms.json"
     if not fileIO(f, "check"):
         print("Creating empty synonyms.json...")
+        fileIO(f, "save", {})
+
+    f = "data/wiki/alerts.json"
+    if not fileIO(f, "check"):
+        print("Creating empty alerts.json...")
         fileIO(f, "save", {})
 
     bot.add_cog(HoggitWiki(bot))
